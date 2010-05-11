cf05d89e6e07924bdaf2727590a966eca7d9b117
bdferris <bdferris@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-05-11 02:10:45 +0000

    undo 1.1.4 release of onebusaway-gtfs-modules, since onebusaway-gtfs-transformer's svn:ignore was not setup properly
    git-svn-id: https://onebusaway.googlecode.com/svn/onebusaway-gtfs-modules@1914 638cd66d-8850-0410-a654-37fb4b726c3a

--- tags/onebusaway-gtfs-modules-1.1.4/onebusaway-gtfs/src/main/java/org/onebusaway/gtfs/services/calendar/CalendarServiceDataFactory.java
+++
@@ -1,7 +0,0 @@
-package org.onebusaway.gtfs.services.calendar;
-
-import org.onebusaway.gtfs.model.calendar.CalendarServiceData;
-
-public interface CalendarServiceDataFactory {
-  public CalendarServiceData createData();
-}
