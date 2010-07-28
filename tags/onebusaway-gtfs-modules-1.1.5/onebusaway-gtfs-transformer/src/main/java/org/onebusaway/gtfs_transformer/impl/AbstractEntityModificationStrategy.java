330b6bf2267401f30fb71561a665002bb418cd55
bdferris <bdferris@638cd66d-8850-0410-a654-37fb4b726c3a>
2010-07-28 15:20:08 +0000

    [maven-release-plugin]  copy for tag onebusaway-gtfs-modules-1.1.5
    git-svn-id: https://onebusaway.googlecode.com/svn/onebusaway-gtfs-modules@2240 638cd66d-8850-0410-a654-37fb4b726c3a

---
+++ tags/onebusaway-gtfs-modules-1.1.5/onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/AbstractEntityModificationStrategy.java
@@ -0,0 +1,39 @@
+package org.onebusaway.gtfs_transformer.impl;
+
+import java.util.Map;
+
+import org.onebusaway.gtfs.csv.schema.BeanWrapper;
+import org.onebusaway.gtfs.csv.schema.BeanWrapperFactory;
+import org.onebusaway.gtfs_transformer.services.ModificationStrategy;
+
+public abstract class AbstractEntityModificationStrategy implements
+    ModificationStrategy {
+
+  private Map<String, Object> _propertyMatches;
+
+  public AbstractEntityModificationStrategy(Map<String, Object> propertyMatches) {
+    _propertyMatches = propertyMatches;
+  }
+
+  protected boolean isModificationApplicable(BeanWrapper wrapped) {
+
+    for (Map.Entry<String, Object> entry : _propertyMatches.entrySet()) {
+      String property = entry.getKey();
+      Object expected = entry.getValue();
+      String[] props = property.split("\\.");
+
+      Object actual = null;
+      BeanWrapper w = wrapped;
+      
+      for (String prop : props) {
+        actual = w.getPropertyValue(prop);
+        w = BeanWrapperFactory.wrap(actual);
+      }
+
+      if ((expected == null && actual != null) || !expected.equals(actual))
+        return false;
+    }
+
+    return true;
+  }
+}
